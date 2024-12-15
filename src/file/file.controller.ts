080611e9eb74b06b56482587a677b1403d5a3d25
dheerajjha <dheeraj.jha@uber.com>
2024-12-16 00:28:47 +0530

    fix build errors

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FileService } from './file.service.js';
 
+interface FileInfo {
+  name: string;
+  type: 'file' | 'directory';
+  size: number;
+  path: string;
+  children?: FileInfo[];
+}
+
 @Controller('api/files')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Get('list/:username')
-  async listFiles(@Param('username') username: string) {
+  async listFiles(@Param('username') username: string): Promise<{ files: FileInfo[] }> {
     return this.fileService.listFiles(username);
   }
 }
